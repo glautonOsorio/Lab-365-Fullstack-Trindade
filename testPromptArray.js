02dc6ceaa4565c881eb9b9025e3f8c19b4942609
Glauton Osório <124407785+glautonOsorio@users.noreply.github.com>
2023-02-05 09:17:00 -0300

    Update testPromptArray.js

--- testPromptArray.js
+++ testPromptArray.js
@@ -1,8 +1,11 @@
+// criando uma variavel de array para botar os nomes e depois fazendo um for loop com o prompt para saber os nomes dos alunos
 let name = Array();
 for (x = 0; x < 5; x++) {
   name[x] = prompt(`Qual o Nome do Aluno(a)?: `, "");
 }
+
 alert("Agora sera as Notas");
+// A mesma coisa que o primeiro loop so que com os numeros
 let number = Array();
 for (y = 0; y < 5; y++) {
   number[y] = prompt(`Qual a nota de cada aluno(a) em Ciência?: `, "");
@@ -15,7 +18,7 @@ let averageStudents =
     parseInt(number[3]) +
     parseInt(number[4])) /
   5;
-
+// Fui obrigado a botar o parseInt pois não estava contando como numero, talvez tenha outra maneira de fazer isso sem o parseInt
 function uni(score, name) {
   if (score >= 1 && score <= 4) {
     return `${name} foi reprovado(a)`;
@@ -25,6 +28,7 @@ function uni(score, name) {
     return `${name}  passou`;
   }
 }
+//Uma function que me deixa saber qual e a nota e nome do aluno pra saber se passou
 
 alert(`A media dos alunos(a) em Ciência ${averageStudents} `);
 
@@ -37,3 +41,4 @@ alert(`O aluno(a) ${uni(number[4], name[4])}`);
 alert(
   "Eu quebrei muito a cabeça pra fazer isso mas consegui de alguma forma yay"
 );
+// alert da felicidade
