bbb4f3c5b4a423dca3d2ca710407a9191ca1f3e0
Glauton <glautonleao@gmail.com>
2023-03-08 05:19:22 -0300

    ex_1_2_3

---
+++ Sixth Week/Ex_1_&_2/answer.js
@@ -0,0 +1,19 @@
+/// [M1S06] Ex 1 - Estrutura de projeto
+
+console.log("  ");
+
+/// [M1S06] Ex 2 - Verbos HTTP
+
+console.log(
+  "Verbo HTTP responsável por capturar dados muito utilizado para listar dados vindo do backend (GET)"
+);
+
+console.log(
+  " Verbo HTTP utilizado para alterar apenas um de seus elementos normalmente envia-se um parâmetro pela URL (PATCH)"
+);
+
+console.log("Verbo HTTP utilizado para alterar todos os seus elementos (PUT)");
+
+console.log(
+  "Verbo HTTP utilizado para enviar dados no corpo da requisição, responsável muitas vezes pelo cadastro de novos itens (POST) "
+);
--- Sixth Week/Ex_3/index.js
+++ Sixth Week/Ex_3/index.js
@@ -9,5 +9,5 @@ const app = express();
 }*/
 
 app.listen(3333, () => {
-  console.log("Servidor Online");
+  console.log("Online we are");
 });
