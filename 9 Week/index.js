7a348682dea02170ef0a8451d763fc69c96e576d
Glauton <glautonleao@gmail.com>
2023-04-03 19:59:15 -0300

    put finished

--- 9 Week/index.js
+++ 9 Week/index.js
@@ -4,7 +4,7 @@ const Place = require("./src/models/place");
 
 const app = express();
 
-app.use(express.json()); //obrigatório
+app.use(express.json());
 
 connection.authenticate();
 connection.sync({ alter: true });
@@ -25,7 +25,7 @@ app.post("/places", async (req, res) => {
     res.status(201).json(place);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Não possivel concluir a operação" });
+    res.status(500).json({ message: "There was a error" });
   }
 });
 
@@ -33,13 +33,34 @@ app.get("/places", async (req, res) => {
   try {
     const places = await Place.findAll();
     return res.json(places);
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: "There was a error" });
+  }
 });
 
 app.delete("/places/:id", async (req, res) => {
   try {
     await Place.destroy({ where: { id: parseInt(req.params.id) } });
     return res.status(200).json({ Message: "The place was deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: "There was a error" });
+  }
+});
+
+app.put("/places/:id", async (req, res) => {
+  try {
+    const placeData = await Place.findByPk(req.params.id);
+    if (!placeData) {
+      return res.status(404).json({ message: "Place not found" });
+    }
+    placeData.name = req.body.name;
+    placeData.contact = req.body.contact;
+    placeData.opening_hours = req.body.opening_hours;
+    placeData.description = req.body.description;
+    placeData.latitude = req.body.latitude;
+    placeData.longitude = req.body.longitude;
+    await placeData.save();
+    res.json(placeData);
   } catch (error) {
     return res.status(500).json({ message: "There was a error" });
   }
